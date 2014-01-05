af454af874c1ea0c20160c9ba2706c0ea7a223c8
Ryan Shoff <ryanshoff@gmail.com>
2014-01-05 17:27:45 -0600

    added euclidean cluster extraction

--- src/4143blob.cpp
+++ src/4143blob.cpp
@@ -13,8 +13,10 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/extract_indices.h>
+#include <pcl/kdtree/kdtree.h>
 #include <4143blob.h>
 #include <pcl/common/time.h>
 #include <pcl/common/impl/angles.hpp>
@@ -165,7 +167,34 @@ class PclBlob
           // tint found plane green for ground
         }
 
-/*
+// Creating the KdTree object for the search method of the extraction
+  pcl::search::KdTree<pcl::PointXYZRGBA>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGBA>);
+  tree->setInputCloud (temp_cloud2);
+
+  std::vector<pcl::PointIndices> cluster_indices;
+  pcl::EuclideanClusterExtraction<pcl::PointXYZRGBA> ec;
+  ec.setClusterTolerance (0.02); // 2cm
+  ec.setMinClusterSize (100);
+  ec.setMaxClusterSize (25000);
+  ec.setSearchMethod (tree);
+  ec.setInputCloud (temp_cloud2);
+  ec.extract (cluster_indices);
+
+  int j = 0;
+  for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+  {
+    CloudPtr cloud_cluster (new Cloud);
+    for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); pit++)
+      cloud_cluster->points.push_back (temp_cloud2->points[*pit]); //*
+    cloud_cluster->width = cloud_cluster->points.size ();
+    cloud_cluster->height = 1;
+    cloud_cluster->is_dense = true;
+
+    std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size () << " data points." << std::endl;
+    j++;
+  }
+
+/* this is the old pclpyramid code
         for(size_t j = 0 ; j < MAX_LINES && temp_cloud2->size() > MIN_CLOUD_POINTS; j++) 
           // look for x lines until cloud gets too small
         {
